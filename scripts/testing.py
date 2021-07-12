9ff106ff8539fae1d0f6725a00179baac7dd512b
karenjennyli <karenjennyli@gmail.com>
2021-07-12 14:42:01 -0400

    add testing.py

---
+++ scripts/testing.py
@@ -0,0 +1,59 @@
+from DeltaArray import DeltaArray
+import numpy as np
+import time
+
+da = DeltaArray('/dev/tty.usbmodem14201')
+
+# p = [[0.010, 0.010, 0.010, # 1, 2, 3
+#       0.010, 0.010, 0.010, # 4, 5, 6
+#       0.010, 0.010, 0.010, # 7, 8, 9
+#       0.010, 0.010, 0.010]] # 10, 11, 12
+
+# p = [[0.010, 0.010, 0.010, # 1, 2, 3
+#       0.000, 0.000, 0.000, # 4, 5, 6
+#       0.010, 0.010, 0.010, # 7, 8, 9
+#       0.000, 0.000, 0.000]] # 10, 11, 12
+
+# p = [[0.030, 0.030, 0.030, # 1, 2, 3
+#       0.000, 0.000, 0.000, # 4, 5, 6
+#       0.030, 0.030, 0.030, # 7, 8, 9
+#       0.000, 0.000, 0.000]] # 10, 11, 12
+
+# hold yellow cube
+
+# p = [[0.010, 0.014, 0.010, # 1, 2, 3
+#       0.000, 0.000, 0.000, # 4, 5, 6
+#       0.010, 0.014, 0.010, # 7, 8, 9
+#       0.000, 0.000, 0.000]] # 10, 11, 12
+
+positions = []
+
+positions.append([[0.000, 0.000, 0.000, # 1, 2, 3
+                   0.000, 0.000, 0.000, # 4, 5, 6
+                   0.000, 0.000, 0.000, # 7, 8, 9
+                   0.000, 0.000, 0.000]]) # 10, 11, 12
+
+positions.append([[0.010, 0.010, 0.010, # 1, 2, 3
+                   0.010, 0.010, 0.010, # 4, 5, 6
+                   0.010, 0.010, 0.010, # 7, 8, 9
+                   0.010, 0.010, 0.010]]) # 10, 11, 12
+
+duration = [1.0]
+
+da.reset()
+da.wait_until_done_moving()
+print(da.get_joint_positions())
+
+# da.move_joint_position(np.array(p), duration)
+# da.wait_until_done_moving()
+# print(da.get_joint_positions())
+
+for p in positions:
+    da.move_joint_position(np.array(p), duration)
+    da.wait_until_done_moving()
+    print(da.get_joint_positions())
+
+# da.reset()
+# da.wait_until_done_moving()
+# print(da.get_joint_positions())
+da.close()
